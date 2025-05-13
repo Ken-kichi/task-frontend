0086470ae1c98dc8b32da56ef149155d0961baea
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2025-05-13 19:12:50 +0900

    APIのURL部分を修正

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function LoginPage() {
-  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
   const router = useRouter();
   const {
     register,
