0a2f3e308b6a6199e17480ebf1a09022f7f04e3c
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2025-05-13 22:07:49 +0900

    console.logを追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,6 +8,7 @@ import { useForm } from 'react-hook-form';
 
 export default function LoginPage() {
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+  console.log(API_BASE_URL);
   const router = useRouter();
   const {
     register,
