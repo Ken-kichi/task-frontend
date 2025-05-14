da78fcf2b637e5eb621992e4ccd013f398dc7577
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2025-05-14 19:23:53 +0900

    Revert "フックスを追加"
    This reverts commit 10e52857cfc158e095387ee2ce61c840515f6de1.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,53 @@
 'use client';
-import useLogin from '@/hooks/useLogin';
+import { LoginProps } from '@/types';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function LoginPage() {
-  const { register, handleSubmit, errors, loginError, onSubmit } = useLogin();
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginProps>();
+  const [loginError, setLoginError] = useState<string | null>(null);
+
+  const onSubmit = async (data: LoginProps) => {
+    setLoginError(null);
+    const formData = new URLSearchParams();
+    formData.append('username', data.username);
+    formData.append('password', data.password);
+
+    try {
+      const response = await axios.post(`${API_BASE_URL}/auth/token`, formData, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        withCredentials: true,
+      });
+
+      if (response.status === 200) {
+        const expireDate = new Date(new Date().getTime() + 15 * 60 * 1000);
+
+        Cookies.set('token', response.data.access_token, {
+          expires: expireDate,
+          sameSite: 'strict',
+        });
+        Cookies.set('user', JSON.stringify(response.data.user), {
+          expires: expireDate,
+          sameSite: 'strict',
+        });
+        router.push('/tasks');
+      }
+    } catch {
+      setLoginError('Login failed. Incorrect user name or password.');
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-sm">
